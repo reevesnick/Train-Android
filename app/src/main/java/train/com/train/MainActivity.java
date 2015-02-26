3c34665f35e8169043448edab69fde534a01238d
Quis <quiscb@gmail.com>
2015-02-25 23:10:09 -0500

    xmls uploaded

--- app/src/main/java/train/com/train/MainActivity.java
+++ app/src/main/java/train/com/train/MainActivity.java
@@ -4,8 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import com.parse.ParseObject;
+import com.parse.Parse;
 
 
 public class MainActivity extends ActionBarActivity {
